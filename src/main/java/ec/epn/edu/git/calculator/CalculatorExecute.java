713232c6ba51e29a1586bfb4fa82864f426e0b23
MRGonorrea79 <jaangulo04@gmail.com>
2025-10-17 12:17:37 -0500

    add CalculatorExecute add calculator archiver .jar

---
+++ src/main/java/ec/epn/edu/git/calculator/CalculatorExecute.java
@@ -0,0 +1,13 @@
+package ec.epn.edu.git.calculator;
+
+public class CalculatorExecute {
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+        int addition = c.addition(4,7);
+        System.out.println(addition);
+
+        int subtraction = c.subtraction(4,7);
+        System.out.println(subtraction);
+    }
+}
